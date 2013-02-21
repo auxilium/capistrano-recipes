9fd275d07a6951dc27ca489da780932bf8aa2009
Manuel van Rijn <manuel@manuelles.nl>
2013-02-21 14:52:43 +0100

    use stream instead of run for tail -f commands

--- lib/capistrano-recipes/recipes/log.rb
+++ lib/capistrano-recipes/recipes/log.rb
@@ -2,14 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../helpers')
 
 Capistrano::Configuration.instance.load do
   namespace :log do
-    def tail file
-      trap("INT") { puts 'Interupted'; exit 0; }
-      run "tail -f #{file}" do |channel, stream, data|
-        puts "#{channel[:host]}: #{data}"
-        break if stream == :err
-      end
-    end
-
     desc "tails the rails log as default"
     task :default, :roles => :app do
       rails
@@ -17,19 +9,22 @@ Capistrano::Configuration.instance.load do
 
     desc "tails the rails log"
     task :rails, :roles => :app do
-      tail "#{shared_path}/log/#{rails_env}.log"
+      trap("INT") { puts 'Interupted'; exit 0; }
+      stream "tail -f #{shared_path}/log/#{rails_env}.log"
     end
 
     if exists?(:sidekiq_cmd)
       desc "tails the SideKiq log"
       task :sidekiq, :roles => :app do
-        tail "#{shared_path}/log/sidekiq.log"
+        trap("INT") { puts 'Interupted'; exit 0; }
+        stream "tail -f #{shared_path}/log/sidekiq.log"
       end
     end
 
     desc "tails the Unicorn log"
     task :unicorn, :roles => :app do
-      tail "#{shared_path}/log/unicorn.log"
+      trap("INT") { puts 'Interupted'; exit 0; }
+      stream "tail -f #{shared_path}/log/unicorn.log"
     end
   end
 end
